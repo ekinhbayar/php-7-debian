aa4e34fffb0fd38756bf38f0348ad62a05ef973e
Ekin H. Bayar <ekin@coproductivity.com>
2016-05-29 08:41:12 -0300

    ... relocating pgsql install, added symlink to php7 ...

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-cd "$(dirname "$0")"
+BASEDIR=$(dirname "$0")
+cd $BASEDIR
 
 # Create a dir for storing PHP module conf
 mkdir /usr/local/php7/etc/conf.d
@@ -17,5 +18,31 @@ cp conf/modules.ini /usr/local/php7/etc/conf.d/modules.ini
 cp conf/php7-fpm.init /etc/init.d/php7-fpm
 chmod +x /etc/init.d/php7-fpm
 update-rc.d php7-fpm defaults
+
+# START POSTGRESQL (PDO) INSTALLATION
+# you can check if you have it already via:
+# /usr/local/php7/bin/php -m | grep pgsql
+
+cd php-src/ext/pdo_pgsql
+./configure --with-pdo-pgsql=/usr/local --with-php-config=/usr/local/php7/bin/php-config
+make
+sudo make install
+
+sudo echo "extension=pdo_pgsql.so" >> /usr/local/php7/lib/php.ini
+
+cd ../pgsql
+./configure --with-pgsql=/usr/local --with-php-config=/usr/local/php7/bin/php-config
+make
+sudo make install
+
+sudo echo "extension=pgsql.so" >> /usr/local/php7/lib/php.ini
+
+# /usr/local/php7/bin/php -m | grep pgsql should now print:
+# $ pdo_pgsql
+# $ pgsql
+
+# Create symlink to php7
+ln -s /usr/local/php7/bin/php /usr/local/bin/php7
+chmod +x /usr/local/bin/php7
 
 service php7-fpm start
